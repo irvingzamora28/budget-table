d07627c1d3bd1b2e85106041bb51b17b6860fa93
Irving Zamora <irvingzamora28@gmail.com>
2024-09-17 11:16:08 -0600

    style: Update layout structure to put a light gray background

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,12 +5,12 @@ import Header from "./Header";
 
 const Layout = () => {
     return (
-        <>
+        <div className="bg-slate-100">
             <Header />
-            <main className="container mx-auto px-4 py-6">
+            <main className="container mx-auto px-4 py-6 h-screen">
                 <Outlet />
             </main>
-        </>
+        </div>
     );
 };
 
