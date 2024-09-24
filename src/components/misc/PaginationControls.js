430ad654fe5a35c6c750cb3537b2d2088b658e92
Irving Zamora <irvingzamora28@gmail.com>
2024-09-24 01:24:31 -0600

    style: Update pagination control button sizes for improved visual consistency.

--- src/components/misc/PaginationControls.js
+++ src/components/misc/PaginationControls.js
@@ -14,28 +14,28 @@ const PaginationControls = ({ currentPage, totalPages, onPageChange }) => {
                 onClick={() => onPageChange(1)}
                 disabled={currentPage === 1}
             >
-                <FaAngleDoubleLeft className="h-5 w-5 text-gray-500" />
+                <FaAngleDoubleLeft className="h-4 w-4 text-gray-500" />
             </button>
             <button
                 className="px-3 py-2 bg-gray-100 border-r border-gray-300 hover:bg-gray-200"
                 onClick={() => onPageChange(currentPage - 1)}
                 disabled={currentPage === 1}
             >
-                <FaChevronLeft className="h-5 w-5 text-gray-500" />
+                <FaChevronLeft className="h-4 w-4 text-gray-500" />
             </button>
             <button
                 className="px-3 py-2 bg-gray-100 border-r border-gray-300 hover:bg-gray-200"
                 onClick={() => onPageChange(currentPage + 1)}
                 disabled={currentPage === totalPages}
             >
-                <FaChevronRight className="h-5 w-5 text-gray-500" />
+                <FaChevronRight className="h-4 w-4 text-gray-500" />
             </button>
             <button
                 className="px-3 py-2 bg-gray-100 hover:bg-gray-200 rounded-br-md rounded-tr-md border-gray-300"
                 onClick={() => onPageChange(totalPages)}
                 disabled={currentPage === totalPages}
             >
-                <FaAngleDoubleRight className="h-5 w-5 text-gray-500" />
+                <FaAngleDoubleRight className="h-4 w-4 text-gray-500" />
             </button>
         </div>
     );
